0753bc9625177a781c55d60c185f20d9f7496ad5
tahmidmahdi <tahmid5550@gmail.com>
2021-05-05 21:52:59 +0600

    spinner added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import MainNav from './component/MainNav/MainNav';
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,26 +13,52 @@ import "aos/dist/aos.css";
 import RecentProjects from './component/RecentProjects/RecentProjects';
 import Blogs from './component/Blogs/Blogs';
 import Contract from './component/Contract/Contract';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from 'react-loader-spinner';
+
 
 
 function App() {
+  const [spinner, setSpinner] = useState(true)
+  useEffect(() => {
+    setTimeout(() => setSpinner(false), 3000)
+  }, []);
+
   return (
-    
-      <Router>
+
+    <Router>
+      {
+        spinner && <Loader className="loader"
+          type="Bars"
+          color="#56FFDA"
+          height={100}
+          width={100}
+          timeout={4000} //3 secs
+
+        />
+      }
+      { !spinner && 
         <Switch>
           <Route exact path="/">
+            {
+              spinner && <Loader
+                type="Puff"
+                color="#00BFFF"
+                height={100}
+                width={100}
+                timeout={3000} //3 secs
+              />
+            }
             <MainNav></MainNav>
             <Home>
-            
             </Home>
             <About></About>
             <RecentProjects></RecentProjects>
             <Blogs></Blogs>
             <Contract></Contract>
           </Route>
-        </Switch>
-      </Router>
-    
+        </Switch>}
+    </Router>
   );
 }
 
